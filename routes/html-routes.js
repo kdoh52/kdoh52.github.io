08ad973eb9808a219456fc33c18ec00a50008e9e
kdoh52 <kdoh52@gmail.com>
2020-10-25 21:11:32 -0700

    html routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,15 @@
 module.exports = (app) => {
-
-    app.get("/", (req, res) => {
-    //   res.render("login");
-    res.send("hi");
-    });
+    
+    // app.get("/", (req, res) => {
+    //     //   res.render("login");
+    //     res.send("hello world");
+    //     console.log("hello")
+    // });
+      
+    // app.get("/about", (req, res) => {
+    //     // res.json("hihihi");
+    //     console.log('haoweifaoweifj')
+    // });
   
 };
   
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require("express");
+let express = require("express");
+var path = require("path");
+// const router = require("express").Router();
 
-const PORT = process.env.PORT || 3000
+let PORT = process.env.PORT || 3000
 
-const app = express();
+let app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,7 +12,24 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //routes
-app.use(require("./routes/html-routes"));
+// app.use(require("./routes/html-routes"));
+
+// app.get("/", (req, res) => {
+//   //   res.render("login");
+//   // res.send("hello world");
+//   // res.sendFile(path.join(__dirname, "/public/index.html"));
+//   console.log("hello")
+// });
+
+app.get("/about", (req, res) => {
+  // res.json("hihihi");
+  res.sendFile(path.join(__dirname, "/public/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  // res.json("hihihi");
+  res.sendFile(path.join(__dirname, "/public/contact.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
